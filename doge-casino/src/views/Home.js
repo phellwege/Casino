cbb039a206dbfe51c1141b931e949b5baa8c3ed9
phellweg <peter.hellwege@vizientinc.com>
2022-12-05 10:05:01 -0700

    skeleton starting

---
+++ doge-casino/src/views/Home.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from "react";
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+export default () => {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
+
