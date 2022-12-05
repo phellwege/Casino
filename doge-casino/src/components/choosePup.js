69e9fb457c0d5e4b6a33555020d2036438ac880c
phellweg <peter.hellwege@vizientinc.com>
2022-12-05 11:17:50 -0700

    started to add ui

--- doge-casino/src/components/choosePup.js
+++ doge-casino/src/components/choosePup.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css/bundle";
+import { Pagination, Lazy} from "swiper";
+import doge1 from '../static/doge.PNG';
+import doge2 from '../static/doge2.PNG';
+import doge3 from '../static/doge3.PNG';
+import doge4 from '../static/doge4.PNG';
+import doge5 from '../static/doge5.PNG';
+import './choosePup.css'
 
 export default () => {
     return (
         <>
-            <div className="page_wrap" id='choosePip'>
-
+            <div className="page_wrap" id='choosePup'>
+                
+                <Swiper
+                    pagination={{
+                    dynamicBullets: true,
+                    }}
+                    modules={[Pagination, Lazy]}
+                    className="dogeSwiper"
+                >
+                    <SwiperSlide>
+                        <div className="DogeSelectionWrap">
+                            <h3>OG Doge</h3>
+                            <br/>
+                            <img src={doge1} className='dogePortrait' loading='lazy'/>
+                            <button>Select</button>
+                        </div>  
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="DogeSelectionWrap">
+                            <h3>OG Doge</h3>
+                            <br/>
+                            <img src={doge2} className='dogePortrait' loading='lazy'/>
+                            <button>Select</button>
+                        </div>  
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="DogeSelectionWrap">
+                            <h3>OG Doge</h3>
+                            <br/>
+                            <img src={doge3} className='dogePortrait' loading='lazy'/>
+                            <button>Select</button>
+                        </div>  
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="DogeSelectionWrap">
+                            <h3>OG Doge</h3>
+                            <br/>
+                            <img src={doge4} className='dogePortrait' loading='lazy'/>
+                            <button>Select</button>
+                        </div>  
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="DogeSelectionWrap">
+                            <h3>OG Doge</h3>
+                            <br/>
+                            <img src={doge5} className='dogePortrait' loading='lazy'/>
+                            <button>Select</button>
+                        </div>  
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </>
     )
--- doge-casino/src/views/Home.js
+++ doge-casino/src/views/Home.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import Header from '../components/header';
 import Footer from '../components/footer';
+import ChoosePup from "../components/choosePup";
 
 export default () => {
     return (
         <>
-            <Header/>
-            <Footer/>
+            {/* <Header/> */}
+                <ChoosePup/>
+            {/* <Footer/> */}
         </>
     )
 }
